1767a143060a15401fd06db6977143c72d1b2299
Deokhaeng <101077014+Deokhaeng@users.noreply.github.com>
2022-04-28 16:14:06 +0900

    CHORE: Default 글자 색 변경

--- src/elements/Text.js
+++ src/elements/Text.js
@@ -9,7 +9,7 @@ const Text = (props) => {
 
 const DefaultText = styled.p`
   ${({ size }) => (size ? `font-size: ${size};` : `font-size: 18px;`)};
-  ${({ color }) => (color ? `color: ${color};` : `color: #485460;`)};
+  ${({ color }) => (color ? `color: ${color};` : `color: #282828;`)};
   ${({ margin }) => (margin ? `margin: ${margin};` : "margin: 0;")};
   ${({ padding }) => (padding ? `padding: ${padding};` : "padding: 0;")};
   ${({ fontWeight }) =>
