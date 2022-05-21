9e33e52912fab94bccf6b1b6fd9b025a3d09ae0f
Jayteeee <wjdxo9928@gmail.com>
2022-05-22 00:24:31 +0900

    PULL: 사이드바 수정 적용

--- src/redux/modules/detailphoto.js
+++ src/redux/modules/detailphoto.js
@@ -6,8 +6,8 @@ import axios from "axios";
 import { getToken } from "../../shared/Token";
 import { DummyData } from "../../shared/DummyData";
 
-// const BASE_URL = "https://doremilan.shop";
-const BASE_URL = "http://52.79.130.222";
+const BASE_URL = "https://doremilan.shop";
+// const BASE_URL = "http://52.79.130.222";
 
 const initialState = {
   nowPhotoDetail: [],
