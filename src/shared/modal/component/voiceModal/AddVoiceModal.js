702164b9c01385d0902cd2b85e4bef5389ec23b4
Jayteeee <wjdxo9928@gmail.com>
2022-05-25 11:16:41 +0900

    FEAT: 소켓 오류 수정

--- src/shared/modal/component/voiceModal/AddVoiceModal.js
+++ src/shared/modal/component/voiceModal/AddVoiceModal.js
@@ -88,7 +88,10 @@ const AddVoiceModal = ({ onClose, familyId, voiceAlbumId }) => {
     }
     // 마이크 사용 권한 획득
     navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-      const mediaRecorder = new MediaRecorder(stream);
+      let options = {
+        mimeType: `audio/webm`,
+      };
+      const mediaRecorder = new MediaRecorder(stream, options);
       mediaRecorder.start();
       setStream(stream);
       setMedia(mediaRecorder);
@@ -128,7 +131,108 @@ const AddVoiceModal = ({ onClose, familyId, voiceAlbumId }) => {
       setOnRec(true);
       setSound(URL.createObjectURL(e.data)); // File 정보 출력
       setVoiceFile(new File([e.data], "file", { type: e.data.type }));
+
+      // const fr = new FileReader();
+      // console.log(fr);
+      // fr.readAsDataURL(e.data);
+      // fr.onload = (e) => {
+      //   console.log(fr.result);
+      // };
+
+      //   console.log(audioBufferToWav(e.data));
+
+      //   function audioBufferToWav(aBuffer) {
+      //     let numOfChan = aBuffer.numberOfChannels,
+      //       btwLength = aBuffer.length * numOfChan * 2 + 44,
+      //       btwArrBuff = new ArrayBuffer(btwLength),
+      //       btwView = new DataView(btwArrBuff),
+      //       btwChnls = [],
+      //       btwIndex,
+      //       btwSample,
+      //       btwOffset = 0,
+      //       btwPos = 0;
+      //     setUint32(0x46464952); // "RIFF"
+      //     setUint32(btwLength - 8); // file length - 8
+      //     setUint32(0x45564157); // "WAVE"
+      //     setUint32(0x20746d66); // "fmt " chunk
+      //     setUint32(16); // length = 16
+      //     setUint16(1); // PCM (uncompressed)
+      //     setUint16(numOfChan);
+      //     setUint32(aBuffer.sampleRate);
+      //     setUint32(aBuffer.sampleRate * 2 * numOfChan); // avg. bytes/sec
+      //     setUint16(numOfChan * 2); // block-align
+      //     setUint16(16); // 16-bit
+      //     setUint32(0x61746164); // "data" - chunk
+      //     setUint32(btwLength - btwPos - 4); // chunk length
+
+      //     console.log(numOfChan);
+
+      //     for (btwIndex = 0; btwIndex < aBuffer.numberOfChannels; btwIndex++)
+      //       btwChnls.push(aBuffer.getChannelData(btwIndex));
+
+      //     while (btwPos < btwLength) {
+      //       for (btwIndex = 0; btwIndex < numOfChan; btwIndex++) {
+      //         // interleave btwChnls
+      //         btwSample = Math.max(
+      //           -1,
+      //           Math.min(1, btwChnls[btwIndex][btwOffset])
+      //         ); // clamp
+      //         btwSample =
+      //           (0.5 + btwSample < 0 ? btwSample * 32768 : btwSample * 32767) | 0; // scale to 16-bit signed int
+      //         btwView.setInt16(btwPos, btwSample, true); // write 16-bit sample
+      //         btwPos += 2;
+      //       }
+      //       btwOffset++; // next source sample
+      //     }
+
+      //     let wavHdr = lamejs.WavHeader.readHeader(new DataView(btwArrBuff));
+      //     let wavSamples = new Int16Array(
+      //       btwArrBuff,
+      //       wavHdr.dataOffset,
+      //       wavHdr.dataLen / 2
+      //     );
+
+      //     blobToMp3(wavHdr.channels, wavHdr.sampleRate, wavSamples);
+
+      //     function setUint16(data) {
+      //       btwView.setUint16(btwPos, data, true);
+      //       btwPos += 2;
+      //     }
+
+      //     function setUint32(data) {
+      //       btwView.setUint32(btwPos, data, true);
+      //       btwPos += 4;
+      //     }
+      //   }
     };
+
+    // // ---------------------------
+
+    // function blobToMp3(channels, sampleRate, samples) {
+    //   var buffer = [];
+    //   var mp3enc = new lamejs.Mp3Encoder(channels, sampleRate, 128);
+    //   var remaining = samples.length;
+    //   var samplesPerFrame = 1152;
+    //   for (var i = 0; remaining >= samplesPerFrame; i += samplesPerFrame) {
+    //     var mono = samples.subarray(i, i + samplesPerFrame);
+    //     var mp3buf = mp3enc.encodeBuffer(mono);
+    //     if (mp3buf.length > 0) {
+    //       buffer.push(new Int8Array(mp3buf));
+    //     }
+    //     remaining -= samplesPerFrame;
+    //   }
+    //   var d = mp3enc.flush();
+    //   if (d.length > 0) {
+    //     buffer.push(new Int8Array(d));
+    //   }
+
+    //   var mp3Blob = new Blob(buffer, { type: "audio/mp3" });
+    //   // var bUrl = window.URL.createObjectURL(mp3Blob);
+
+    //   // send the download link to the console
+    //   // console.log("mp3 download:", bUrl);
+    //   return console.log(mp3Blob)
+    // }
 
     // -------------------------------- 테스트
     // let channels = 1; //1 for mono or 2 for stereo
